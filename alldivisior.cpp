34aae9e706cdd1df63ca72bfb1185ad827c06fd5
Ishita rawat <ishitarawat2004@gmail.com>
2025-03-24 23:23:26 +0530

    Update alldivisior.cpp

--- alldivisior.cpp
+++ alldivisior.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
-
-
-}
+int n;
+  cin>>n;
+  vector<int>ans;
+  for(int i=2;i<=sqrt(n);i++){
+    if(n%i==0){
+      int s=n/i;
+      ans.push_back(i);
+      if(s!=i){
+        ans.push_back(s);
+    }}
+sort(ans.begin(),ans.end();
+  
+  for(int i=0;i<ans.size();i++){
+    cout<<ans[i]<<" ";
+  }
+  }
